2286375e922eb6109b2a74952aefe7e4b515c8b1
sabourisadra <sabouri.sadra@gmail.com>
2021-06-10 20:19:49 +0430

    add : Transmitter.v added.

---
+++ Hardware/Transmitter/Transmitter.v
@@ -0,0 +1,23 @@
+`include "Scrambler/Scrambler.v"
+
+module Transmitter(Start, Input, Reset, Clock, Output);
+/*
+ * Module `Transmitter`
+ *
+ * 802.11a IEEE Transmitter
+ * 
+ * Sub modules:
+ * + Scrambler/Scrambler.v
+ *
+ * parameters:
+ * Start    [1]: Active high one-shot start
+ * Input    [1]: Input data stream
+ * Reset    [1]: Active high asynchronous reset
+ * Clock    [1]: Input clock
+ * Output   [1]: Output Wifi data frame
+ *
+ *************************************************
+ * @author : sadrasabouri(sabouri.sadra@gmail.com)
+ *************************************************
+ */
+endmodule
