ad28d1d402878e3d5a5c4151567e8af16b1804ac
sabourisadra <sabouri.sadra@gmail.com>
2021-06-12 01:26:10 +0430

    update : tests updated accordingly.

--- Hardware/Transmitter/Transmitter_tb.v
+++ Hardware/Transmitter/Transmitter_tb.v
@@ -41,13 +41,16 @@ module Transmitter_tb;
 		$display($time, "ns |",
 			" Reset=%b", Reset,
 			" Input=%b", Input,
+            " Start=%b", Start,
 			" --- >",
 			" Output:%b", Output);
     end
 
     integer i = 0;
+    integer j = 0;
     integer N_PASS = 0;
-    reg [1:127] DESIRED_OUT = 127'b0000111011110010110010010000001000100110001011101011011000001100110101001110011110110100001010101111101001010001101110001111111;
+    reg [1:288] DESIRED_OUT = 288'b101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010110100000000100000000000000011101111001001011001000000100010011000101110101101100000110011010100111001111011010000101010111110100101000110111000111111100001110111100101100100100000010001001100;
+    reg [0:3] DATA_INPUT = 4'b1001;
 
     initial
     begin
@@ -61,24 +64,46 @@ module Transmitter_tb;
 
         Reset = 1;
         #1;
+        
         Reset = 0;
-
-        for (i = 1; i <= 127; i = i + 1)
+        Start = 1;
+        #1;
+        
+        Start = 0;
+        #1;
+        
+        for (i = 1; i <= 288; i = i + 1)
         begin
             if (DESIRED_OUT[i] == Output)
             begin
-                $display("[OK] (", i, "/", 127, ")");
+                $display("[OK] (", i, "/", 288, ")");
                 N_PASS = N_PASS + 1;  
             end
             else
-                $display("[FAILED] (", i, "/", 127, ") Expected:%b  |  Got:%b", DESIRED_OUT[i], Output);
+                $display("[FAILED] (", i, "/", 288, ") Expected:%b  |  Got:%b", DESIRED_OUT[i], Output);
             #1;
         end
 
-        if (N_PASS == 127)
+        if (N_PASS == 288)
             $display("ALL TEST PASSED. :)");
         else
-            $display(127 - N_PASS, " test(s) failed. :(");
+            $display(288 - N_PASS, " test(s) failed. :(");
+    end
+
+    initial
+    begin
+		#10
+        #1;
+        
+        #137;
+        $display("[DATA_INPUT::START]");
+        for (j = 0; j < 4; j = j + 1)
+        begin
+            Input = DATA_INPUT[j];
+            #1;
+        end
+        Input = 0;
+        $display("[DATA_INPUT::END]");
     end
 endmodule
 
