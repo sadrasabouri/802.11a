2d17eda95ca6b9c3603f53e473f5d1349df61d25
sabourisadra <sabouri.sadra@gmail.com>
2021-06-11 10:30:23 +0430

    add : Receiver verilog module added.

---
+++ Hardware/Receiver/Receiver.v
@@ -0,0 +1,22 @@
+`include "DeScrambler/DeScrambler.v"
+
+module Transmitter(Input, Reset, Clock, Output);
+/*
+ * Module `Receiver`
+ *
+ * 802.11a IEEE Receiver
+ * 
+ * Sub modules:
+ * + DeScrambler/DeScrambler.v
+ *
+ * parameters:
+ * Input    [1]: Input Wifi frame
+ * Reset    [1]: Active high asynchronous reset
+ * Clock    [1]: Input clock
+ * Output   [1]: Output data
+ *
+ *************************************************
+ * @author : sadrasabouri(sabouri.sadra@gmail.com)
+ *************************************************
+ */
+endmodule
