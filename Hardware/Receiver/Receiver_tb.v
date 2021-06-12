beac90abf2f0544242cd27147f63aca5e6dab254
sabourisadra <sabouri.sadra@gmail.com>
2021-06-12 14:42:49 +0430

    update : Receiver test bench updated.

--- Hardware/Receiver/Receiver_tb.v
+++ Hardware/Receiver/Receiver_tb.v
@@ -24,7 +24,7 @@ module Receiver_tb;
 		.Clock(Clock),
 		.Reset(Reset),
 		.Output(Output),
-        .Error(Error),
+        .Error(Error)
     );
 	
     localparam CLK_PERIOD = 0.5;
@@ -47,7 +47,6 @@ module Receiver_tb;
     end
 
     integer i = 0;
-    integer N_PASS = 0;
     reg [1:288] SEQ_IN = 288'b101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010110100000000100000000000000011101111001001011001000000100010011000101110101101100000110011010100111001111011010000101010111110100101000110111000111111100001110111100101100100100000010001001100;
 
     initial
@@ -66,27 +65,37 @@ module Receiver_tb;
         for (i = 2; i <= 288; i = i + 1)
         begin
             Input = SEQ_IN[i];
-            // if (Output == 0)
-            // begin
-            //     $display("[OK] (", i-1, "/", 127, ")");
-            //     N_PASS = N_PASS + 1;  
-            // end
-            // else
-            //     $display("[FAILED] (", i-1, "/", 127, ") Expected:%b  |  Got:%b", 1'b0, Output);
+            #1;
+        end
+    end
+
+    integer j = 0;
+    integer N_PASS = 0;
+    reg [1:4] DESIRED_OUT = 4'b1001;
+    initial
+    begin
+        #10;
+        #1;
+
+        #141;
+        $display("[DATA::START]");
+        for (j = 1; j <= 4; j = j + 1)
+        begin
+            if (Output == DESIRED_OUT[j])
+            begin
+                $display("[OK] (", j, "/", 4, ")");
+                N_PASS = N_PASS + 1;  
+            end
+            else
+                $display("[FAILED] (", j, "/", 4, ") Expected:%b  |  Got:%b", DESIRED_OUT[j], Output);
             #1;
         end
 
-        // if (Output == 0)
-        // begin
-        //     $display("[OK] (", i-1, "/", 127, ")");
-        //     N_PASS = N_PASS + 1;  
-        // end
-        // else
-        //     $display("[FAILED] (", i-1, "/", 127, ") Expected:%b  |  Got:%b", 1'b0, Output);
-        
-        // if (N_PASS == 127)
-        //     $display("ALL TEST PASSED. :)");
-        // else
-        //     $display(127 - N_PASS, " test(s) failed. :(");
+        if (N_PASS == 4)
+            $display("ALL TEST PASSED. :)");
+        else
+            $display(4 - N_PASS, " test(s) failed. :(");
+
+        $display("[DATA::END]");
     end
 endmodule
