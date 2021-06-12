0a10b98d418f19b6344a7272eedbddb4b85d45dd
sabourisadra <sabouri.sadra@gmail.com>
2021-06-12 13:23:01 +0430

    update : Receiver updated.

--- Hardware/Receiver/Receiver.v
+++ Hardware/Receiver/Receiver.v
@@ -25,10 +25,94 @@ module Receiver(Input, Reset, Clock, Output);
 
     output wire Output;
 
+    //  DeScrambler Instatiation:
+    reg descrambler_in;
+    reg descrambler_reset;
+    reg [7:1] descrambler_init;
     DeScrambler descrambler(
-        .Input(Input),
-        .Reset(Reset),
+        .Input(descrambler_in),
+        .Reset(descrambler_reset),
+        .Init(descrambler_init),
         .Clock(Clock),
         .Output(Output)
     );
+
+    parameter MAX_TURNS_PLCP_PREAMBLE = 95; //  = 12 * 8 - 1 (12 Symbols, each symbol's a byte)
+    
+    reg [0:MAX_TURNS_PLCP_PREAMBLE] Input_buffer;
+    always @(posedge Clock, posedge Reset)  //  Save last 96 bit for preambles
+        if (Reset)
+            Input_buffer <= 95'b0;
+        else
+            Input_buffer <= {{Input_buffer[1:MAX_TURNS_PLCP_PREAMBLE]}, {Input}};
+
+
+    //  Wifi-Frame - Parameters:
+    //      Current state register:
+    reg [3:0] CURRENT_STATE;
+    //      IDLE state:
+    parameter [3:0] IDLE_STATE = 0;
+    parameter [0:8*12-1] PREAMBLE_SYMBOLS = {8'hAA, 8'hAA, 8'hAA, 8'hAA,
+                                             8'hAA, 8'hAA, 8'hAA, 8'hAA,
+                                             8'hAA, 8'hAA, 8'hAA, 8'hAA};
+
+    //      Signal state:
+    //          Rate state:
+    parameter [3:0] SIGNAL_RATE_STATE = 2;
+    reg [1:0] TURNS_RATE_STATE;
+    reg [0:3] RATE;                      // [Data Rate (Big Endian)]
+    //          Reserved:
+    parameter [3:0] SIGNAL_RESERVERD_STATE = 3;
+    //          LENGTH:
+    parameter [3:0] SIGNAL_LENGTH_STATE = 4;
+    reg [3:0] TURNS_LENGTH_STATE;
+    reg [0:11] LENGTH;                  // [Octet Numbers of Data (Big Endian)]
+    //          PARITY:
+    parameter [3:0] SIGNAL_PARITY_STATE = 5;
+    //          TAIL:
+    parameter [3:0] SIGNAL_TAIL_STATE = 6;
+    reg [2:0] TURNS_TAIL_STATE;
+    //      Data state:
+    //          Service:
+    parameter [3:0] DATA_SERVICE_STATE = 7;
+    reg [3:0] TURNS_SERVICE_STATE;
+    //          PSDU:
+    parameter [3:0] DATA_PSDU_STATE = 8;
+    reg [14:0] TURNS_PSDU_STATE;            //  Maximum is 8 * (2 ^ 12 - 1)
+    //          Tail:
+    parameter [3:0] DATA_TAIL_STATE = 9;
+    //          PAD_BITS:
+
+
+    always @(posedge Clock, posedge Reset)
+    begin
+        if (Reset)
+        begin
+            descrambler_in <= 1'b0;
+            descrambler_init <= 7'b0000000;
+            CURRENT_STATE <= IDLE_STATE;
+            TURNS_RATE_STATE <= 2'b00;
+            RATE <= 4'b1101;                // =6
+            TURNS_LENGTH_STATE <= 4'h0;
+            LENGTH <= 12'h010;              // =16
+            TURNS_TAIL_STATE <= 3'b000;
+            TURNS_SERVICE_STATE <= 4'b0000;
+            TURNS_PSDU_STATE <= 15'b000_0000_0000_0000;
+        end
+        else
+        begin
+            case (CURRENT_STATE)
+                IDLE_STATE:
+                begin
+                    if (Input_buffer == PREAMBLE_SYMBOLS)
+                        CURRENT_STATE <= SIGNAL_RATE_STATE;
+                end 
+                default:
+                begin
+                    if (Input_buffer == PREAMBLE_SYMBOLS)
+                        CURRENT_STATE <= SIGNAL_RATE_STATE;
+                end 
+            endcase
+        end
+    end
 endmodule
