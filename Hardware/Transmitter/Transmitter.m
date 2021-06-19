47bcefb43e17b2823c5b338b5873cc3982460163
sabourisadra <sabouri.sadra@gmail.com>
2021-06-19 19:47:05 +0430

    update : Transmitter.m updated.

--- Hardware/Transmitter/Transmitter.m
+++ Hardware/Transmitter/Transmitter.m
@@ -6,14 +6,14 @@ function Output = Transmitter(Input)
 %  *
 %  * parameters:
 %  * Input    [n]: Input data array 
-%  * Output   [n]: Output descrambled data array
+%  * Output   [m]: Output descrambled data array
 %  *
 %  *************************************************
 %  * @author : sadrasabouri(sabouri.sadra@gmail.com)
 %  *************************************************
 %  */
     Symbol = [1,0,1,0, 1,0,1,0];                                            %=0hAA
-    PREAMBLE_SYMBOLS = [Symbol, Symbol, Symbol, Symbol,  Symbol, Symbol, Symbol, Symbol,  Symbol, Symbol, Symbol, Symbol];
+    PREAMBLE_SYMBOLS = [Symbol, Symbol, Symbol, Symbol,  Symbol, Symbol, Symbol, Symbol,  Symbol, Symbol, Symbol, Symbol]; %   PLCP Preamble
     RATE = [1,1,0,1];
     LENGTH = [0,0,0,0, 0,0,0,1, 0,0,0,0];                                   %=0h010
     N_DBPS = 24;
@@ -21,36 +21,40 @@ function Output = Transmitter(Input)
     DATA_LENGH = 8 * bin2dec(num2str(LENGTH));
     N_SYM = ceil((16 + DATA_LENGH + 6) / N_DBPS);
     N = ceil(size(Input, 2) / DATA_LENGH);
-    Output = zeros(1, N * (120 + N_SYM * N_DBPS));
+
+    Output = [];
     
     %   For each packet:
     for i=1:N
-        output = zeros(1, 120 + N_SYM * N_DBPS);
+        toCoder = zeros(1, (24 + N_SYM * N_DBPS));
         
-        output(1:96) = PREAMBLE_SYMBOLS;                                    %   PLCP Preamble
-        output(97:100) = RATE;                                              %   SIGNAL.RATE
-        output(101) = 0;                                                    %   SIGNAL.RESERVED
-        output(102:113) = LENGTH;                                           %   SIGNAL.LENGTH
-        if (mod(sum(output(1:113)), 2) == 1)                                %   SIGNAL.PARITY
-            output(114) = 1;
+        toCoder(1:4) = RATE;                                                 %   SIGNAL.RATE
+        toCoder(5) = 0;                                                      %   SIGNAL.RESERVED
+        toCoder(6:17) = LENGTH;                                              %   SIGNAL.LENGTH
+        if (mod(sum([PREAMBLE_SYMBOLS, toCoder(1:17)]), 2) == 1)             %   SIGNAL.PARITY
+            toCoder(18) = 1;
         else
-            output(114) = 0;
+            toCoder(18) = 0;
         end
-        output(115:120) = zeros(1, 6);                                      %   SIGNAL.TAIL
+        toCoder(19:24) = zeros(1, 6);                                        %   SIGNAL.TAIL
         
 
-        output(121:136) = zeros(1, 16);                                     %   DATA.SERVICE
-        if (size(Input, 2) < DATA_LENGH*i)                                  %   DATA.PSDU
-            output(137:136+DATA_LENGH) = [Input(DATA_LENGH*(i-1)+1:end), zeros(1, DATA_LENGH - (size(Input, 2) - DATA_LENGH*(i-1)))];
+        toCoder(25:40) = zeros(1, 16);                                       %   DATA.SERVICE
+        if (size(Input, 2) < DATA_LENGH*i)                                   %   DATA.PSDU
+            toCoder(41:40+DATA_LENGH) = [Input(DATA_LENGH*(i-1)+1:end), zeros(1, DATA_LENGH - (size(Input, 2) - DATA_LENGH*(i-1)))];
         else
-            output(137:136+DATA_LENGH) = Input(DATA_LENGH*(i-1)+1:DATA_LENGH*i);
+            toCoder(41:40+DATA_LENGH) = Input(DATA_LENGH*(i-1)+1:DATA_LENGH*i);
         end
-        output(137+DATA_LENGH:137+DATA_LENGH+5) = zeros(1, 6);              %   DATA.TAIL
+        toCoder(41+DATA_LENGH:41+DATA_LENGH+5) = zeros(1, 6);              %   DATA.TAIL
         
         cd Scrambler;   %   Including Scrambler
-        output(121:end) = Scrambler(output(121:end));
+        toCoder(25:end) = Scrambler(toCoder(25:end));
         cd ..;          %   Comming back to main directory
-        
-        Output((120 + N_SYM * N_DBPS) * (i-1) + 1:(120 + N_SYM * N_DBPS) * i) = output;
+
+        cd ConvEncoder;
+        toCoder = [PREAMBLE_SYMBOLS, ConvEncoder(toCoder(1:end))];
+        cd ..;
+
+        Output = [Output, toCoder];
     end
 end
