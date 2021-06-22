473ea2365cceb55433459767a8930871b4ea32c6
sabourisadra <sabouri.sadra@gmail.com>
2021-06-22 19:03:36 +0430

    add : FIND_MIN added.

--- Hardware/Receiver/ViterbiDecoder/ViterbiDecoder.v
+++ Hardware/Receiver/ViterbiDecoder/ViterbiDecoder.v
@@ -28,7 +28,7 @@ module ViterbiDecoder(Input, Reset, Clock, Output);
 
 
     reg inBuff;
-    reg [9:0] input_counter;
+    reg [9:0] counter;
     //  Input buffer handler:
     //      Buffer would be = {inBuff, Input} for more efficiency
     always @(posedge Clock, posedge Reset)
@@ -45,8 +45,13 @@ module ViterbiDecoder(Input, Reset, Clock, Output);
     //      In this section branch metrics are calculated for each
     parameter BPMC = 0;
     reg collect_buffer;
-    //  1. TRCBCK (Trace Back Calculation)
-    parameter TRCBCK = 1;
+    //  1. FIND_MIN (Find Minimum Value of Paths)
+    parameter FIND_MIN = 1;
+    reg [0:7] min_dist;
+    reg [5:0] INDEX_MIN;
+    //  2. TRCBCK (Trace Back Stage)
+    parameter TRCBCK = 2;
+
 
     //  Viterbit - Graph
     integer i;
@@ -57,12 +62,14 @@ module ViterbiDecoder(Input, Reset, Clock, Output);
             CURRENT_SATE <= BPMC;
             inBuff <= 2'b00;
             collect_buffer <= 1'b0;
-            for(i = 0; i < 64; i = i + 1)   //  Costs and Path Initilizing
+            for(i = 0; i < 64; i = i + 1)               //  Costs and Path Initilizing
             begin
                 CostsTilNow[i] <= 8'hff;
                 Path[i] <= {MAX_LENGTH{7'b1000000}};    //  Unknow state    
             end
-            input_counter <= 10'b00_0000_0000;
+            counter <= 10'b00_0000_0000;
+            min_dist <= 8'baa;                          //  Maximum bit error
+            INDEX_MIN <= 6'b000000;  
         end
         else
         begin
@@ -70,7 +77,7 @@ module ViterbiDecoder(Input, Reset, Clock, Output);
                 BPMC:
                     if (collect_buffer)
                     begin
-                        if (input_counter == 10'b00_0000_0000)  //  First State
+                        if (counter == 10'b00_0000_0000)  //  First State
                         begin
                             //  State 0 -> 0  | Output : 00
                             Path[6'b000000] <= {7'b0000000, Path[6'b000000][7:MAX_LENGTH-1]};
@@ -80,16 +87,16 @@ module ViterbiDecoder(Input, Reset, Clock, Output);
                             Path[6'b100000] <= {7'b0000000, Path[6'b100000][7:MAX_LENGTH-1]};
                             CostsTilNow[6'b100000] <= $unsigned((inBuff ^ 1'b1) + (Input ^ 1'b1));
                         end
-                        else if (input_counter < MAX_LENGTH)
+                        else if (counter < MAX_LENGTH)
                         begin
                             for(i = 0; i < 64; i = i + 1)   //  Costs and Path Initilizing
                             begin
                                 //  If there exits a path to i <just> from {i[4:0], 0}
-                                if      (Path[{i[4:0], 1'b0}][(input_counter-1)*7 +: 7] != 7'b1000000 &&
-                                         Path[{i[4:0], 1'b1}][(input_counter-1)*7 +: 7] == 7'b1000000)
+                                if      (Path[{i[4:0], 1'b0}][(counter-1)*7 +: 7] != 7'b1000000 &&
+                                         Path[{i[4:0], 1'b1}][(counter-1)*7 +: 7] == 7'b1000000)
                                 begin
                                     //                               Known, frome state 
-                                    Path[i][input_counter*7 +: 7] <= {1'b0, i[4:0], 1'b0};
+                                    Path[i][counter*7 +: 7] <= {1'b0, i[4:0], 1'b0};
                                     
                                     //  Hamming Distance Calculation:
                                     //                  Cost till now                             XOR(In(0), O(0)=(x1+x3+x4+x6+x7))   +      XOR(In(1), O(1)=(x1+x2+x3+x4+x7))
@@ -100,11 +107,11 @@ module ViterbiDecoder(Input, Reset, Clock, Output);
                                     //     CostsTilNow[{i[4:0], 1'b0}] + $unsigned({1'b0, ^{inBuff, i[5], i[3], i[2], i[0], 1'b0}} + {1'b0, ^{Input, i[5], i[4], i[3], i[2], 1'b0}}));
                                 end
                                 //  If there exits a path to i <just> from {i[4:0], 1}
-                                else if (Path[{i[4:0], 1'b0}][(input_counter-1)*7 +: 7] == 7'b1000000 &&
-                                         Path[{i[4:0], 1'b1}][(input_counter-1)*7 +: 7] != 7'b1000000)
+                                else if (Path[{i[4:0], 1'b0}][(counter-1)*7 +: 7] == 7'b1000000 &&
+                                         Path[{i[4:0], 1'b1}][(counter-1)*7 +: 7] != 7'b1000000)
                                 begin
                                     //                               Known, frome state 
-                                    Path[i][input_counter*7 +: 7] <= {1'b0, i[4:0], 1'b1};
+                                    Path[i][counter*7 +: 7] <= {1'b0, i[4:0], 1'b1};
                                     
                                     //  Hamming Distance Calculation:
                                     //                  Cost till now                             XOR(In(0), O(0)=(x1+x3+x4+x6+x7))   +      XOR(In(1), O(1)=(x1+x2+x3+x4+x7))
@@ -115,12 +122,12 @@ module ViterbiDecoder(Input, Reset, Clock, Output);
                                     //     CostsTilNow[{i[4:0], 1'b1}] + $unsigned({1'b0, ^{inBuff, i[5], i[3], i[2], i[0], 1'b1}} + {1'b0, ^{Input, i[5], i[4], i[3], i[2], 1'b1}}));
                                 end
                                 //  If there exits a path to i from both from {i[4:0], 1} and {i[4:0], 0} 
-                                else if (Path[{i[4:0], 1'b0}][(input_counter-1)*7 +: 7] != 7'b1000000 &&
-                                         Path[{i[4:0], 1'b1}][(input_counter-1)*7 +: 7] != 7'b1000000)
+                                else if (Path[{i[4:0], 1'b0}][(counter-1)*7 +: 7] != 7'b1000000 &&
+                                         Path[{i[4:0], 1'b1}][(counter-1)*7 +: 7] != 7'b1000000)
                                 begin
                                     if (CostsTilNow[{i[4:0], 1'b0}] + $unsigned({1'b0, ^{inBuff, i[5], i[3], i[2], i[0], 1'b0}} + {1'b0, ^{Input, i[5], i[4], i[3], i[2], 1'b0}}) > CostsTilNow[{i[4:0], 1'b1}] + $unsigned({1'b0, ^{inBuff, i[5], i[3], i[2], i[0], 1'b1}} + {1'b0, ^{Input, i[5], i[4], i[3], i[2], 1'b1}}))
                                     begin   //  {i[4:0], 1'b1} is better path than {i[4:0], 1'b0}
-                                        Path[i][input_counter*7 +: 7] <= {1'b0, i[4:0], 1'b1};
+                                        Path[i][counter*7 +: 7] <= {1'b0, i[4:0], 1'b1};
                                         CostsTilNow[i] <= CostsTilNow[{i[4:0], 1'b1}] + $unsigned({1'b0, ^{inBuff, i[5], i[3], i[2], i[0], 1'b1}} + {1'b0, ^{Input, i[5], i[4], i[3], i[2], 1'b1}});
                                         
                                         // $display("[INNER_TEST::0,1:1]: [%b]->[%b] | I=%d | O(%d)=%d --> new cost=%d", {i[4:0], 1'b1}, i[5:0], {inBuff, Input}, i[5],
@@ -129,7 +136,7 @@ module ViterbiDecoder(Input, Reset, Clock, Output);
                                     end
                                     else
                                     begin   //  {i[4:0], 1'b0} is better path than {i[4:0], 1'b1} 
-                                        Path[i][input_counter*7 +: 7] <= {1'b0, i[4:0], 1'b0};
+                                        Path[i][counter*7 +: 7] <= {1'b0, i[4:0], 1'b0};
                                         CostsTilNow[i] <= CostsTilNow[{i[4:0], 1'b0}] + $unsigned({1'b0, ^{inBuff, i[5], i[3], i[2], i[0], 1'b0}} + {1'b0, ^{Input, i[5], i[4], i[3], i[2], 1'b0}});
                                         
                                         // $display("[INNER_TEST::0,1:0]: [%b]->[%b] | I=%d | O(%d)=%d --> new cost=%d", {i[4:0], 1'b0}, i[5:0], {inBuff, Input}, i[5],
@@ -143,13 +150,30 @@ module ViterbiDecoder(Input, Reset, Clock, Output);
                         end
                         else
                         begin
-                            CURRENT_SATE <= TRCBCK;
-                            input_counter <= 10'b00_0000_0000;
+                            CURRENT_SATE <= FIND_MIN;
+                            counter <= 10'b00_0000_0000;
                         end
-                        input_counter <= input_counter + 10'b00_0000_0001;
+                        counter <= counter + 10'b00_0000_0001;
                     end
+                FIND_MIN:   // Find Minimum Value of Paths
+                begin
+                    if (counter < 10'b00_0001_0000)
+                    begin
+                        if (CostsTilNow[counter] < min_dist)
+                        begin
+                            min_dist <= CostsTilNow[counter];
+                            INDEX_MIN <= counter;
+                        end
+                    end
+                    begin
+                        CURRENT_SATE <= TRCBCK;
+                        counter <= 10'b00_0000_0000;
+                    end
+                    counter <= counter + 10'b00_0000_0001;
+                end
                 TRCBCK:
                 begin
+                    
                 end
                 default: 
                 begin
@@ -161,7 +185,9 @@ module ViterbiDecoder(Input, Reset, Clock, Output);
                         CostsTilNow[i] <= 8'b1111_1111;
                         Path[i] <= {MAX_LENGTH{7'b1000000}};    //  Unknow state
                     end
-                    input_counter <= 10'b00_0000_0000;
+                    counter <= 10'b00_0000_0000;
+                    min_dist <= 8'baa;                          //  Maximum bit error
+                    INDEX_MIN <= 6'b000000;
                 end
             endcase
             collect_buffer <= ~collect_buffer;
